99c8cc95bfdbae0dfac5481808317cad3c7f4ec0
Juliandrimikhael <Juliandri.mikhael@gmail.com>
2017-08-07 16:23:16 +0700

    done :')

--- Num.js
+++ Num.js
@@ -1,7 +1,10 @@
-function sum (a,b,c){
-  return (a+b+c);
+function sum (){
+  var result = 0
+  for (x=0; x<arguments.length; x++ ){
+      result += arguments[x];
+  }
+  console.log(result)
 }
 
-
-
-console.log(sum(1,2,7));
+sum (1,2,7)
+sum (5,6)
