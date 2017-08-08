9960a06e95973ac0916c8d3d54ea6f71a4adaef8
Firmansyah <firmansyahas@hotmail.com>
2017-08-08 14:44:12 +0700

    update

---
+++ Num.js
@@ -0,0 +1,9 @@
+function sum(){
+            var total =  0;
+            for(var i=0;i<arguments.length;i++)  {
+              total = total + arguments[i];
+               }
+                  console.log(total)
+  }
+sum(1,2,7);
+sum(5,6);
